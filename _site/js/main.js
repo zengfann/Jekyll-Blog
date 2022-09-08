f4e940de246999ec0edfea7b6b7a4befc0708d16
曾凡 <zf2420940650@gmail.com>
2022-09-08 15:01:04 +0800

    update

--- _site/js/main.js
+++ _site/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   $('a.blog-button').click(function() {
     // If already in blog, return early without animate overlay panel again.
-    if (location.hash && location.hash == "#blog") return;
+    if ((location.hash && location.hash == "#blog")) return;
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
     $('.main-post-list').removeClass('hidden');
     currentWidth = $('.panel-cover').width();
@@ -13,6 +13,23 @@ $(document).ready(function() {
       $('.panel-cover').animate({'max-width': '700px', 'width': '30%'}, 400, swing = 'swing', function() {} );
     }
   });
+
+  
+  $('a.about-button').click(function() {
+    // If already in about, return early without animate overlay panel again.
+    if ((location.hash && location.hash == "#about")) return;
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    $('.main-post-list').removeClass('hidden');
+    currentWidth = $('.panel-cover').width();
+    // if (currentWidth < 960) {
+    //   $('.panel-cover').addClass('panel-cover--collapsed');
+    // } else {
+    //   $('.panel-cover').css('max-width',currentWidth);
+    //   $('.panel-cover').animate({'max-width': '700px', 'width': '30%'}, 400, swing = 'swing', function() {} );
+    // }
+  });
+
+
 
   if (window.location.hash && window.location.hash == "#blog") {
     $('.panel-cover').addClass('panel-cover--collapsed');
---
+++ about.md
@@ -0,0 +1,7 @@
+---
+layout: post
+title: Hello World - Vno
+date: 2016-02-16 15:32:24.000000000 +09:00
+---
+
+## zenfann
